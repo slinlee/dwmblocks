bdec06c1d08593fe73f138434102ac94c55960a2
Slin Lee <slin@slinlee.com>
2023-02-22 20:11:29 -0800

    Disable some unused blocks

--- config.h
+++ config.h
@@ -3,9 +3,9 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	/* {"⌨", "sb-kbselect", 0, 30}, */
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	{"",	"sb-tasks",	10,	26},
+	// {"",	"sb-tasks",	10,	26},
 	{"",	"sb-music",	0,	11},
-	{"",	"sb-pacpackages",	0,	8},
+	// {"",	"sb-pacpackages",	0,	8},
 	{"",	"sb-news",		0,	6},
 	/* {"",	"sb-price lbc \"LBRY Token\" 📚",			9000,	22}, */
 	/* {"",	"sb-price bat \"Basic Attention Token\" 🦁",	9000,	20}, */
@@ -19,13 +19,13 @@ static const Block blocks[] = {
 	/* {"",	"sb-moonphase",	18000,	17}, */
 	{"",	"sb-doppler",	0,	13},
 	{"",	"sb-forecast",	18000,	5},
-	{"",	"sb-mailbox",	180,	12},
+	// {"",	"sb-mailbox",	180,	12},
 	{"",	"sb-nettraf",	1,	16},
 	{"",	"sb-volume",	0,	10},
 	{"",	"sb-battery",	5,	3},
 	{"",	"sb-clock",	60,	1},
 	{"",	"sb-internet",	5,	4},
-	{"",	"sb-help-icon",	0,	15},
+	// {"",	"sb-help-icon",	0,	15},
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
