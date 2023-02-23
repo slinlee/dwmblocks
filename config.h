66f9cd961eea53154643c81d6b09e3d4d329732e
Slin Lee <slin@slinlee.com>
2023-02-22 20:57:01 -0800

    Disable music block

--- config.h
+++ config.h
@@ -4,7 +4,7 @@ static const Block blocks[] = {
 	/* {"⌨", "sb-kbselect", 0, 30}, */
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	// {"",	"sb-tasks",	10,	26},
-	{"",	"sb-music",	0,	11},
+	// {"",	"sb-music",	0,	11},
 	// {"",	"sb-pacpackages",	0,	8},
 	{"",	"sb-news",		0,	6},
 	/* {"",	"sb-price lbc \"LBRY Token\" 📚",			9000,	22}, */
